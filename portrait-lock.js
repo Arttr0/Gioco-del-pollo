9688df56c01acc0408ab81ee03b1b94f348432b6
Arttr0 <142749957+Arttr0@users.noreply.github.com>
2025-09-16 13:06:46 +0300

    Fix2

--- portrait-lock.js
+++ portrait-lock.js
@@ -1,11 +1,19 @@
+function isMobile() {
+    return /Mobi|Android/i.test(navigator.userAgent);
+}
+
 function checkOrientation() {
-    if(window.innerHeight < window.innerWidth) {
-        // Горизонтальная ориентация
-        document.getElementById('unity-canvas').style.display = 'none';
+    if(isMobile() && window.innerHeight < window.innerWidth) {
+        // Горизонтальная ориентация на мобилке
+        canvas.style.display = 'none';
+        loadingBar.style.display = 'none';
+        warningBanner.style.display = 'none';
         document.getElementById('orientation-warning').style.display = 'block';
     } else {
-        // Портретная ориентация
-        document.getElementById('unity-canvas').style.display = 'block';
+        // Портретная ориентация или ПК
+        canvas.style.display = 'block';
+        loadingBar.style.display = 'block';
+        warningBanner.style.display = 'block';
         document.getElementById('orientation-warning').style.display = 'none';
     }
 }
